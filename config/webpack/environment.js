68af77d7725c924a85ff566fcca0a5f8914c49bb
taishi0502 <kimutai_71@icloud.com>
2022-06-02 18:06:38 +0900

    webpack検証

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,24 +7,5 @@ environment.plugins.prepend('Provide',
         jQuery: 'jquery/src/jquery'
     })
 )
-/*Fightボタン*/ 
-var animateButton = function(e) {
 
-    e.preventDefault;
-    //reset animation
-    e.target.classList.remove('animate');
-    
-    e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
-
-/*以上Fightボタン*/ 
 module.exports = environment
