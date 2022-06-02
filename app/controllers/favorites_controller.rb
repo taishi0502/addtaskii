241e2e75f05d12329d39ff8b4998815dfd8742b4
taishi0502 <kimutai_71@icloud.com>
2022-06-02 16:35:49 +0900

    いいね機能の実装2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
     @mission = Mission.find(params[:mission_id])
-    favorite = current_user.favorites.new(mission_id: mission.id)
+    favorite = current_user.favorites.new(mission_id: @mission.id)
     favorite.save
   end
 
   def destroy
     @mission = Mission.find(params[:mission_id])
-    favorite = current_user.favorites.find_by(mission_id: mission.id)
+    favorite = current_user.favorites.find_by(mission_id: @mission.id)
     favorite.destroy
   end
 end
